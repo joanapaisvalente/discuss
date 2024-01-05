b60752966b62316f4f489abd4df67f28ac6140a8
Joana Valente <Joana.Valente@ctw.bmwgroup.com>
2024-01-05 15:44:30 +0000

    cache to deduplicate requests

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,19 +1,23 @@
 import type { Comment } from "@prisma/client";
 import { db } from "@/db";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: { name: true, image: true },
+/* with the addition of cache, we can call this function as many times as we want and in as many
+components as we want that as long as the arguments are identical, call will be deduplicated ->
+the function will only be executed one single time as long as the arguments are the same.*/
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: { name: true, image: true },
+        },
       },
-    },
-  });
-}
+    });
+  }
+);
